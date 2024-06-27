20da6b88d26f5f9a957fadfc7b6cdddf50bc24ee
Menu Temesgen <menutemesgen@gmail.com>
2024-06-27 09:44:46 +0300

    added some text.

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -5,17 +5,17 @@ time_bound = input("Is it time-bound? (yes/no): ").lower()
 match priority:
   case "high":
     if time_bound == "yes":
-       print(f"'{task}' is a high priority task that requires immediate attention today!")  
+       print(f"Reminder: '{task}' is a high priority task that requires immediate attention today!")  
     elif time_bound == "no":
-       print(f"'{task}' is a high priority task. but it is not time bounded.")
+       print(f"Note: '{task}' is a high priority task. but it is not time bounded.")
   case "medium":
     if time_bound == "yes":
-      print(f"'{task}' is a medium priority task, but it is time bounded.")
+      print(f"Reminder: '{task}' is a medium priority task, but it is time bounded.")
     elif time_bound == "no":
-      print(f"'{task}' is a medium priority task and it is not time bounded.")
+      print(f"Note: '{task}' is a medium priority task and it is not time bounded.")
   case "low":
     if time_bound == "yes":
-      print(f"'{task}' is a low priority task, but it is time bounded.")
+      print(f"Note: '{task}' is a low priority task, but it is time bounded.")
     elif time_bound == "no":
-      print(f"'{task}' is a low priority task. Consider completing it when you have free time.")
+      print(f"Note: '{task}' is a low priority task. Consider completing it when you have free time.")
 
