e9ca77d42f8bab155b5875445927d2cbfc41ee41
Menu Temesgen <menutemesgen@gmail.com>
2024-07-05 23:06:02 +0300

    created the explore_datetime.py

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,17 @@
+import datetime
+
+def display_current_datetime():
+    current_date = datetime.datetime.now()
+    formatted_current_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    print (f"Current date and time: {formatted_current_date}")
+
+display_current_datetime()
+
+number_of_days = int (input ("Enter the number of days to add to the current date: "))
+def calculate_future_date(number_of_days):
+    delta = datetime.timedelta(days=number_of_days)
+    current_date = datetime.datetime.now().date()
+    future_date = current_date + delta
+    print(f"Future date: {future_date}")
+    
+calculate_future_date(number_of_days)
